942f7688edf2faac942a42df8059d24b43887318
Aaron Abrook <aabrook@gmail.com>
2017-03-21 18:52:37 +1000

    Add show route

--- web/controllers/distance_tracker_controller.ex
+++ web/controllers/distance_tracker_controller.ex
@@ -7,4 +7,21 @@ defmodule DistanceTracker.Controller do
       |> DistanceTracker.Repo.all
     render(conn, "index.json", trackings: trackings)
   end
+
+  def show(conn, %{"uuid" => uuid}) do
+    with {:ok, tracker} <- IO.inspect(DistanceTracker.Repo.get(DistanceTracker.DistanceTracker, uuid)) do
+      render(conn, "500.json", "Nope!")
+    else
+      {:error, errors} ->
+        render(conn, "500.json", %{errors: errors})
+      nil ->
+        render(conn, DistanceTracker.ErrorView, "404.json", %{error: "Not found"})
+    end
+  end
+
+  def create(conn, params) do
+  end
+
+  def delete(conn, params) do
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule DistanceTracker.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", Controller, :index
+    get "/:uuid", Controller, :show
   end
 
   # Other scopes may use custom stacks.
