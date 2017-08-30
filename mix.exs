e18149e9518226d2c1efcb820fba5329a9197aaa
Aaron Abrook <aabrook@gmail.com>
2017-08-30 21:39:18 +1000

    Add swagger helper alias

--- mix.exs
+++ mix.exs
@@ -51,6 +51,8 @@ defmodule DistanceTracker.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "ecto.migrate", "test"],
+     "swagger": ["phx.swagger.generate priv/static/swagger.json --router DistanceTracker.Router --endpoint DistanceTracker.Endpoint"]
+   ]
   end
 end
