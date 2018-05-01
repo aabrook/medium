638798008046d397b82ca58dc97ceff85b7f2b09
aabrook <aabrook@gmail.com>
2018-05-01 21:11:16 +1000

    Break out queries

--- web/graphql/schema.ex
+++ web/graphql/schema.ex
@@ -4,21 +4,30 @@ defmodule DistanceTracker.Schema do
   alias DistanceTracker.{Tracker, Repo}
 
   import_types(DistanceTracker.Types)
+
   query do
     field :distances, list_of(:distance_tracker) do
-      resolve fn _, _, _ ->
-        distances = Tracker
-          |> DistanceTracker.Repo.all
-
-        {:ok, distances}
-      end
+      resolve &list/3
     end
 
     field :distance, :distance_tracker do
       arg(:id, non_null(:string))
-      resolve fn _, %{id: id}, _ ->
-        {:ok, Repo.get(Tracker, id)}
-      end
+      resolve &show/3
     end
+  end
+
+  defp show(_, %{id: id}, _) do
+    with tracker = %Tracker{} <- Repo.get(Tracker, id) do
+      {:ok, tracker}
+    else
+      nil -> {:error, "Not Found"}
+    end
+  end
+
+  defp list(_, _, _) do
+    distances = Tracker
+      |> DistanceTracker.Repo.all
+
+    {:ok, distances}
   end
 end
